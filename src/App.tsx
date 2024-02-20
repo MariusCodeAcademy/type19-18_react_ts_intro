a14d290469fc499d64b524ef6f6d75a458cd170d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-20 09:39:35 +0200

    person list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import Greet from './components/Greet';
 import Heading from './components/Heading';
 import Person from './components/Person';
+import PersonList from './components/PersonList';
+
+type PersonObj = {
+  firstName: string;
+  lastName: string;
+};
 
 function App() {
-  const perObj = {
+  const perObj: PersonObj = {
     firstName: 'Mike',
     lastName: 'Dow',
   };
+  const personArr: PersonObj[] = [
+    {
+      firstName: 'James',
+      lastName: 'Bond',
+    },
+    {
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+    },
+    {
+      firstName: 'Clark',
+      lastName: 'Kent',
+    },
+  ];
   return (
     <div className='container'>
       <h2 className='display-2'>React TS App</h2>
       <Person item={perObj} />
+      <PersonList list={personArr} />
       <Heading title={'Sveiki'} />
       <Greet name='James' amount={500_000_000} favColor='tomato' />
       <Greet name='Mike' amount={10_000_000} />
