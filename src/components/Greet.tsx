cd46561638c2bba24ba70fe39876e846d4db7d3a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-20 09:02:49 +0200

    Update Greet component styling and add Person component

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -8,7 +8,7 @@ function Greet(props: GreetProps) {
   console.log('props ===', props);
   return (
     <div>
-      <h3 className='alert alert-secondary'>
+      <h3 className='alert alert-secondary '>
         Hello there {props.name}, you have won {props.amount} dollars <br />
         {props.favColor && (
           <span className='fw-normal'>And your favorite color is {props.favColor}</span>
---
+++ src/components/Person.tsx
@@ -0,0 +1,8 @@
+// Person componentas
+// priima 2 props, firstName ir lastName abu stringai
+
+// jsx atvaizduojam su siek tiek stiliaus varda ir pavarde
+
+// importuojam i App.tsx
+
+// (2varijantas) priima objekta kuris turi viduje 2 savybes firstName ir lastName abu stringai
