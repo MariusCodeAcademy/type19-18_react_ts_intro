f3657f0bf3f7e00d91dc5958413bfa88e2b58f83
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-20 08:28:58 +0200

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div className='container'>
+      <h2 className='display-2'>React TS App</h2>
+    </div>
+  );
 }
 
 export default App;
