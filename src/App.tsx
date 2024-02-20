97bfe56988445b649a611cf3cf513c5760c093ea
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-20 08:40:23 +0200

    simple props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+type HeadingProps = {
+  title: string;
+};
+
+function Heading(props: HeadingProps) {
+  console.log('props ===', props);
+  return <h3>{props.title} visi</h3>;
+}
+
 function App() {
   return (
     <div className='container'>
       <h2 className='display-2'>React TS App</h2>
+      <Heading title='Sveiki' />
     </div>
   );
 }
